5f12faff416f2d175babf373b2d8f1a1faa06194
Nalahatra <fitiavananalahatra@gmail.com>
2026-07-23 19:54:08 +0300

    feat: create constant for inchanged stat

---
+++ components/constants.js
@@ -0,0 +1,28 @@
+export const CATEGORY_LABELS = {
+  client: "Client",
+  "client-vip": "Client VIP",
+  "reclamation-client": "Réclamation",
+  facture: "Facture",
+  newsletter: "Newsletter",
+  spam: "Spam",
+  interne: "Interne",
+};
+
+export const CATEGORY_COLORS = {
+  client: "bg-blue-100 text-blue-800",
+  "client-vip": "bg-purple-100 text-purple-800",
+  "reclamation-client": "bg-orange-100 text-orange-800",
+  facture: "bg-green-100 text-green-800",
+  newsletter: "bg-pink-100 text-pink-800",
+  spam: "bg-red-100 text-red-800",
+  interne: "bg-gray-100 text-gray-800",
+};
+
+export function formatDate(iso) {
+  return new Intl.DateTimeFormat("fr-FR", {
+    day: "numeric",
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date(iso));
+}
